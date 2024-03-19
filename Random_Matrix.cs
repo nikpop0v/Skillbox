4ba023fbb95cd42cfe3b0b8153221a01d4c0499a
Nikita Popov <pop0v.nikita2001@yandex.ru>
2024-03-19 12:18:43 +0300

    Start

---
+++ Random_Matrix.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace Matrix1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Случайная Матрица ");
+            Console.Write(" Количество строк: ");
+            var rowCount = int.Parse(Console.ReadLine());
+            Console.Write(" Количество столбцов: ");
+            var colCount = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[rowCount, colCount];
+
+
+            int sum = 0;
+
+            for (var row = 0; row < rowCount; row++)
+            {
+                Console.WriteLine();
+                for (var col = 0; col < colCount; col++)
+                {
+
+                    matrix[row,col] = random.Next(10);
+                    Console.Write($"{matrix[row, col]}\t");
+                }
+                
+            }
+            
+            foreach (int i in matrix)
+            {
+                sum += i;
+            }
+           
+            Console.WriteLine($"\nСумма: " + sum);
+
+
+
+            Console.ReadKey();
+        } 
+    }
+}
---
+++ SumMatrix.cs
@@ -0,0 +1,64 @@
+﻿namespace SummMat2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Console.WriteLine("Сложение Матриц");
+            Console.Write(" Количество строк: ");
+            var rowCount = int.Parse(Console.ReadLine());
+            Console.Write(" Количество столбцов: ");
+            var colCount = int.Parse(Console.ReadLine());
+            int[,] matrix1 = new int[rowCount, colCount];
+            int[,] matrix2 = new int[rowCount, colCount];
+            int[,] matrix3 = new int[rowCount, colCount];
+
+            
+
+            for (var row = 0; row < rowCount; row++)
+            {
+                for (var col = 0; col < colCount; col++)
+                {
+                    matrix1[row, col] = random.Next(10);
+                    matrix2[row, col] = random.Next(10);
+                }
+            }
+
+            for (var row = 0; row < rowCount; row++)
+            {
+                for (var col = 0; col < colCount; col++)
+                {
+                    matrix3[row, col] = matrix1[row, col] + matrix2[row, col];
+                    
+                }
+            }
+
+            Console.WriteLine("\nСложение Матриц:");
+            for (var row = 0; row < rowCount; row++)
+            {
+                for (var col = 0; col < colCount; col++)
+                {
+                    Console.Write($"{matrix1[row, col]}\t");
+                    
+                }
+                Console.Write($"\t\t");
+                for (var col = 0; col < colCount; col++)
+                {
+                    Console.Write($"{matrix2[row, col]}\t");
+                }
+                Console.Write($"\t\t");
+                for (var col = 0; col < colCount; col++)
+                {
+                    Console.Write($"{matrix3[row, col]}\t");
+                    
+                }
+                Console.WriteLine();
+                
+
+            }
+           
+           
+        } 
+}
+}
