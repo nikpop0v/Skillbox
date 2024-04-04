83df252bd940bb5516244b1524bff8c42f03c2d4
Nikita Popov <pop0v.nikita2001@yandex.ru>
2024-04-04 17:00:01 +0300

    method-tasks-add

---
+++ method-tasks/ReversWords/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace ReversWords
+{
+    internal class Program
+    {
+        static string ReverseWords(string[] text) 
+        {
+            Array.Reverse(text); 
+            string addSeparator = string.Join(" ", text); 
+            return addSeparator;
+        }
+        static string SplitWords(string text)  
+        {
+            string[] split = text.Split(' '); 
+            string newText = ReverseWords(split); 
+            return newText;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Фразу:");
+            string sentence = Console.ReadLine();
+            sentence = SplitWords(sentence);
+            Console.WriteLine("Фраза наоборот:");
+            Console.WriteLine(sentence);
+            Console.ReadLine();
+        }
+        
+        
+    }
+}
---
+++ method-tasks/Separation/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace separation
+{
+    internal class Program
+    {
+          public static string[] SplitText(string text)
+          {
+              return text.Split(' ');
+          }
+        static void PrintWords(string[] parts)
+        {
+            foreach (string part in parts)
+            {
+                Console.WriteLine($"Слово: {part} "); 
+            }
+        }
+          static void Main(string[] args)
+          {
+            Console.WriteLine("Введите Фразу:");
+            PrintWords(SplitText(Console.ReadLine()));
+          } 
+    }
+}
+
+
